dc3b06f454086df11c491c69808a71f4fbcd7cb5
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-09-09 19:06:32 -0300

    retirando função de opacidade

--- api.js
+++ api.js
@@ -88,7 +88,3 @@ function toggleHeatmap() {
 function changeRadius() {
   heatmap.set("radius", heatmap.get("radius") ? null : 20);
 }
-
-function changeOpacity() {
-  heatmap.set("opacity", heatmap.get("opacity") ? null : 0.2);
-}
