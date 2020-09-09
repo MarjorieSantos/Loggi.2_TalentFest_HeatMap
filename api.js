326297ffc4b1a99a71ae3fa3adbb806a387fef95
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-09-09 12:30:24 -0300

    mostrando ponto de calor

--- api.js
+++ api.js
@@ -1,13 +1,49 @@
-function initMap() {
+const graphQL = `query AppQuery {
+  closestDrivers(productType: 2, transportType: "1", lat: -23.55, lng: -46.63, radius: 10.0, limit: 200, citySlug:"sp") {
+    driversCount
+    readyDriversCount
+    busyDriversCount
+    drivers {
+      lng
+      lat
+      busy
+    }
+  }
+}`;
+
+const endPoint = 'https://www.loggi.com/graphql';
+
+const consumeLoggiApi = async (endPointGraphql, query, variable = {}) => {
+  const res = await fetch(endPointGraphql,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'ApiKey larissa.miyaji@gmail.com:690606c72dd744f76826a04630a8a16268ac2560',
+      },
+      body: JSON.stringify({ query, variable }),
+    })
+  return res.json();
+}
+
+async function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
     center: { lat: -23.3256, lng: -46.3820 },
     mapTypeId: "hybrid"
   });
 
-  const coords = test.drivers.map((item) => {
-    return new google.maps.LatLng(item.lat, item.lng)
+  const response = await consumeLoggiApi(endPoint, graphQL)
+
+  console.log(response.data)
+
+  const coords = response.data.closestDrivers.drivers.map(heatpoint => {
+    return new google.maps.LatLng(heatpoint.lat, heatpoint.lng)
   })
+
+  // const coords = test.drivers.map((item) => {
+  //   return new google.maps.LatLng(item.lat, item.lng)
+  // })
 
   heatmap = new google.maps.visualization.HeatmapLayer({
     data: coords,
@@ -47,20 +83,9 @@ function changeOpacity() {
   heatmap.set("opacity", heatmap.get("opacity") ? null : 0.2);
 }
 
-const test = {
-  "driversCount": 200,
-  "readyDriversCount": 139,
-  "busyDriversCount": 61,
-  "drivers": [
-    {
-      "lng": -46.63737869262695,
-      "lat": -23.55320167541504,
-      "busy": false
-    },
-    {
-      "lng": -46.420684814453125,
-      "lat": -23.43790054321289,
-      "busy": true
-    },
-  ]
-}
+// consumeLoggiApi(endPoint, graphQL).then(res => {
+//   console.log(res)
+//   res.data.closestDrivers.drivers.map(pontodecalor => {
+//     return console.log(pontodecalor.lat, pontodecalor.lng)
+//   })
+// })
