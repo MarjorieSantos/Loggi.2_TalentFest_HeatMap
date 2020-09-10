a9e65518a0f27b0f2215251fa40af51aa5e4ea5a
Ully Lopes <ullylopes@ymail.com>
2020-09-10 17:33:11 -0300

    retirar o cabeçalho

--- index.js
+++
@@ -1,9 +0,0 @@
-
-//function inicialModal(modal) {
-//    const modal = document.getElementById(modal);
-//    modal.classList.add('mostrar');
-//}
-
-//inicialModal('content');
-
-//export default inicialModal;
